004e68f1185da2b32c7c2c5b6126e536e35ecb3f
Laurens Schaap <l.schaap91@gmail.com>
2023-04-13 11:32:48 +0200

    Huiswerk les 5

--- les5/main.py
+++ les5/main.py
@@ -1,1 +1,19 @@
+"""
+Huiswerk voor les 5 van het vak Python voor NHA Software Ontiwikkelaar
 
+"""
+
+weekmenu = {
+    "maandag": "pizza",
+    "dinsdag": "hamburger",
+    "woensdag": "pasta",
+    "donderdag": "uitsmijter",
+    "vrijdag": "spare ribs",
+    "zaterdag": "zalmfilet",
+    "zondag": "chuck roast",
+}
+
+dag_van_de_week = input('Welke dag van de week is het?: ').lower()
+
+
+print(f"Vandaag staat op het menu: {weekmenu[dag_van_de_week]}")
