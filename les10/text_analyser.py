1835179ccd97a04c59c1bd6cf96f41fc09388a63
Laurens Schaap <l.schaap91@gmail.com>
2023-04-19 13:04:41 +0200

    text analyser uit boek

--- les10/text_analyser.py
+++ les10/text_analyser.py
@@ -12,10 +12,15 @@ def analyse_text(filename):
         IOError: al "filename" niet bestaat
         
     Retouneer:
-        Het aantal regels in het bestand
+        Een tupel met als eerste element het aantal regels in de bestanden en als tweede element het aantal tekens.
     """
+    lines = 0
+    chars = 0
     with open(filename, 'r') as f:
-        return sum(1 for _ in f)
+        for line in f:
+            lines += 1
+            chars += len(line)
+        return (lines, chars)
 
 class TextAnalysisTests(unittest.TestCase):
     
@@ -24,9 +29,9 @@ class TextAnalysisTests(unittest.TestCase):
         self.filename = 'text_analysis_test_file.txt'
         with open(self.filename, 'w') as f:
             f.write('Now we are engaged in a great civil war,\n'
-                    'testing whether that nation, \n'
-                    'or any nation so conceived and so dedicated, \n'
-                    'can long endur.')
+                    'testing whether that nation,\n'
+                    'or any nation so conceived and so dedicated,\n'
+                    'can long endur. ')
             
     def tearDown(self):
         try:
@@ -38,7 +43,18 @@ class TextAnalysisTests(unittest.TestCase):
         analyse_text(self.filename)
         
     def test_line_count(self):
-        self.assertEqual(analyse_text(self.filename), 4)
+        self.assertEqual(analyse_text(self.filename)[0], 4)
+        
+    def test_character_count(self):
+        self.assertEqual(analyse_text(self.filename)[1], 131)
+        
+    def text_no_such_file(self):
+        with self.assertRaises(IOError):
+            analyse_text('foobar')
+            
+    def test_no_deletion(self):
+        analyse_text(self.filename)
+        self.assertTrue(os.path.exists(self.filename))
         
 if __name__ == "__main__":
     unittest.main()
